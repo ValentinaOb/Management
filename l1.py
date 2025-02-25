b1da508b5428291cbc4a4a6434dd0f8e01998783
user <user@WIN-MHGL1PRC8MH>
2025-02-25 11:35:16 +0200

    1

--- l1.py
+++ l1.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-import csv
 
 # Enable interactive mode
 plt.ion()
 
 # Create figure and axis
 fig, ax = plt.subplots()
-ax.set_ylim([0, 8])  # Змінений масштаб для видимих змін
+ax.set_ylim([-100, 100])  # Змінений масштаб для видимих змін
 ax.set_xlim([0, 100])
 ax.grid(True)  # Додає сітку
 
@@ -24,26 +23,25 @@ ax.set_title('Real-time Data Plot')
 ax.legend()
 
 t = 0
+dt = 1
 
-output_data = np.loadtxt("file.csv", delimiter=",", skiprows=1)
-new_data1 = output_data[:, 0] 
-new_data2 = output_data[:, 1] 
+
 data1 = np.array([])
 data2 = np.array([])
 
-data1=np.append(data1, new_data1)
-data2=np.append(data1, new_data2)
-
-#print(new_data1)
 
 line1, = ax.plot([], [], label="X")
 line2, = ax.plot([], [], label="Y")
 
-while True:
-    t += 0.01
-    new_data = float(5 - math.exp(-t))
 
-    print(t,' ', new_data)
+x1 = x0 = x_1 = 0
+u1 = u_1 = 0
+S=0
+plt.legend()
+
+while True:
+    t += dt
+    new_data = float(5 - math.exp(-t))
 
     # Append new data and time
     data = np.append(data, new_data)
@@ -54,12 +52,44 @@ while True:
         data = data[-100:]
         time = time[-100:]
 
+        data1 = data1[-100:]
+        data2 = data2[-100:]
+        
+
     # Update the plot
     line.set_data(time, data)
 
+#
+    u0 = u1
+    u_1 = u0
+
+    x0 = x1
+    x_1 = x0
+
+    e = 5 - math.exp(-t)
+
+    S+=dt*(math.exp(-t)-x0)
+    u1=0.6 * ((e - x0)) + 6 *S+0.1*math.exp(-t) - 0.1*((x1-x0)/dt)
+    u_=(u1-u0)/dt
+    
+    x1 = 6 * x0 + (5 / t * (x1 - x0)) + (1 / t**2 * (x1 - 2 * x0 + x_1)) - 0.1 * u_
+    #print(S, " ", u1, " ", u_," ",x1)
+
+    data1 = np.append(data1, x1)
+    data2 = np.append(data2, u1)
+    
     line1.set_data(time, data1)
     line2.set_data(time, data2)
+    #print(data2)
+  
+    
+    #
+
+
+
 
     ax.set_xlim(time[0], time[-1])  # Adjust x-axis dynamically
+    #ax.set_ylim(data1[0], data1[-1])  # Adjust x-axis dynamically
     plt.draw()
+    
     plt.pause(0.01)
--- tg.py
+++
@@ -1,9 +0,0 @@
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.array([2014,2015,2016,2017,2018,2019, 2020, 2021, 2022, 2023,2024])
-y = np.array([35,50,80,150,200,300,400,550,700,800,900])
-
-plt.plot(x,y)
-plt.show()
