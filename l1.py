b7b7fb1c14c3019a5696404121c201d7a85178e0
user <user@WIN-MHGL1PRC8MH>
2025-02-25 15:09:35 +0200

    1

--- l1.py
+++ l1.py
@@ -7,59 +7,40 @@ plt.ion()
 
 # Create figure and axis
 fig, ax = plt.subplots()
-ax.set_ylim([-100, 100])  # Змінений масштаб для видимих змін
+ax.set_ylim([-100, 100])  
 ax.set_xlim([0, 100])
-ax.grid(True)  # Додає сітку
+ax.grid(True)
 
 # Initialize empty data arrays
 data = np.array([])
 time = np.array([])
+data1 = np.array([])
+data2 = np.array([])
 
-# Create an empty plot and add a legend
+# Create empty plots
 line, = ax.plot([], [], label="Data")
+line1, = ax.plot([], [], label="X")
+line2, = ax.plot([], [], label="Y")
+
 ax.set_xlabel('Time (ms)')
 ax.set_ylabel('Data')
 ax.set_title('Real-time Data Plot')
 ax.legend()
 
-t = 0
+t =0  # Start with a small nonzero value
 dt = 1
-
-
-data1 = np.array([])
-data2 = np.array([])
-
-
-line1, = ax.plot([], [], label="X")
-line2, = ax.plot([], [], label="Y")
-
-
 x1 = x0 = x_1 = 0
 u1 = u_1 = 0
-S=0
-plt.legend()
+S = 0
 
 while True:
     t += dt
     new_data = float(5 - math.exp(-t))
 
-    # Append new data and time
+    # Append new data
     data = np.append(data, new_data)
     time = np.append(time, t)
 
-    # Keep the last 100 points
-    if len(data) > 100:
-        data = data[-100:]
-        time = time[-100:]
-
-        data1 = data1[-100:]
-        data2 = data2[-100:]
-        
-
-    # Update the plot
-    line.set_data(time, data)
-
-#
     u0 = u1
     u_1 = u0
 
@@ -67,29 +48,32 @@ while True:
     x_1 = x0
 
     e = 5 - math.exp(-t)
+    S += dt * (math.exp(-t) - x0)
 
-    S+=dt*(math.exp(-t)-x0)
-    u1=0.6 * ((e - x0)) + 6 *S+0.1*math.exp(-t) - 0.1*((x1-x0)/dt)
-    u_=(u1-u0)/dt
+    u1 = 0.6 * ((e - x0)) + 6 * S + 0.1 * math.exp(-t) - 0.1 * ((x1 - x0) / dt)
+    u_ = (u1 - u0) / dt
     
     x1 = 6 * x0 + (5 / t * (x1 - x0)) + (1 / t**2 * (x1 - 2 * x0 + x_1)) - 0.1 * u_
-    #print(S, " ", u1, " ", u_," ",x1)
 
     data1 = np.append(data1, x1)
     data2 = np.append(data2, u1)
-    
-    line1.set_data(time, data1)
-    line2.set_data(time, data2)
-    #print(data2)
-  
-    
-    #
 
+    # Keep only the last 100 points
+    if len(data) > 100:
+        data = data[-100:]
+        time = time[-100:]
+        data1 = data1[-100:]
+        data2 = data2[-100:]
 
+    # Ensure all arrays have the same length
+    min_len = min(len(time), len(data1), len(data2))
+    line.set_data(time[:min_len], data[:min_len])
+    line1.set_data(time[:min_len], data1[:min_len])
+    line2.set_data(time[:min_len], data2[:min_len])
 
+    # Adjust X-axis dynamically
+    if len(time) > 0:
+        ax.set_xlim(time[0], time[-1])
 
-    ax.set_xlim(time[0], time[-1])  # Adjust x-axis dynamically
-    #ax.set_ylim(data1[0], data1[-1])  # Adjust x-axis dynamically
     plt.draw()
-    
     plt.pause(0.01)
